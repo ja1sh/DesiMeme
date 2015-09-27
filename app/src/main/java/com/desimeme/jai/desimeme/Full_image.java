00dc9075710ec4c06103b797afc92f385be7c26c
Jai Sharma <jaisharmafps2@gmail.com>
2015-09-27 10:55:26 +0530

    if statment removed | still getting blank image for sharing!

--- app/src/main/java/com/desimeme/jai/desimeme/Full_image.java
+++ app/src/main/java/com/desimeme/jai/desimeme/Full_image.java
@@ -1,5 +1,6 @@
 package com.desimeme.jai.desimeme;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
@@ -42,69 +43,48 @@ public class Full_image extends ActionBarActivity {
         return true;
     }
 
-    public boolean isExternalStorageWritable() {
-        String state = Environment.getExternalStorageState();
-        if (Environment.MEDIA_MOUNTED.equals(state)) {
-            return true;
+    private boolean shouldReturnResult() {
+        Intent intent = getIntent();
+        if (intent!=null && intent.getType()!=null) {
+            return intent.getType().indexOf("*/*") != -1;
+        } else {
+            return false;
         }
-        return false;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-       // if (id == R.id.action_share) {
-         //   return true;
-        //}
-
         // Handle item selection
+        ImageView image = (ImageView) findViewById(R.id.full_image_view);
+        Bitmap bitmap = ((BitmapDrawable) image.getDrawable()).getBitmap();
+
+        File sd = Environment.getExternalStorageDirectory();
+        String fileName = "desi.png";
+        File dest = new File(sd, fileName);
+        try {
+            FileOutputStream out;
+            out = new FileOutputStream(dest);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
+            out.flush();
+            out.close();
+        } catch (FileNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
         switch (item.getItemId()) {
             case R.id.action_share:
-                // Handle item selection
-                ImageView image = (ImageView) findViewById(R.id.full_image_view);
-                Bitmap bitmap = ((BitmapDrawable)image.getDrawable()).getBitmap(); //file to be saved as per user selection
-                if (isExternalStorageWritable()) {
-                    String sd = Environment.getExternalStorageDirectory().getAbsolutePath();
-                    File ssd = new File(sd, "share");
-                    if (!ssd.exists())
-                        ssd.mkdirs();
-                    String fileName = "temp"; //saved as png file
-                    File dest = new File(ssd, fileName + ".jpg");
-                    FileOutputStream out = null;
-                    if (dest.canWrite()) {
-                        dest.setWritable(true);
-                    }
-                    try {
-                        out = new FileOutputStream(dest);
-                        bitmap.compress(Bitmap.CompressFormat.JPEG, 100, out); // bmp is your Bitmap instance
-                        // PNG is a lossless format, the compression factor (100) is ignored
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    } finally {
-                        try {
-                            if (out != null) {
-                                out.close();
-                            }
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
-                    }
+                Uri uri = Uri.fromFile(dest);
 
-                    Uri uri = Uri.fromFile(dest);
                     Intent shareIntent = new Intent();
                     shareIntent.setAction(Intent.ACTION_SEND);
-                    shareIntent.setType("*/*");
                     shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
-                    startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.action_share))); //shared via Intent
+                    shareIntent.setType("*/*");
+                    startActivity(Intent.createChooser(shareIntent, getResources().getText(R.string.action_share)));
                 }
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
+        return true;
+
         }
     }
-
-}
-
